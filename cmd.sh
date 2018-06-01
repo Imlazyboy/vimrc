391503892a464d9702d040380c8fa9db8e09182b
zhuguangjun2002 <zhuguangjun2002@163.com>
2018-06-01 15:51:14 +0800

    添加了vim8安装更新;添加`vim-atuoformat`插件

--- cmd.sh
+++ cmd.sh
@@ -34,8 +34,56 @@ sudo add-apt-repository ppa:jonathonf/vim
 sudo apt update
 sudo apt install vim
 
+#选址一个支持`python2`,支持`clipboard`的安装包为默认安装包：
+sudo update-alternatives --config vim
+
+#我们一般选择`vim.gnome-py2`,它比较符号我们的要求。
+
+#=====================================================================
+#Selection    Path                    Priority   Status
+#------------------------------------------------------------
+#0            /usr/bin/vim.gnome       60        auto mode
+#1            /usr/bin/vim.basic       30        manual mode
+#2            /usr/bin/vim.gnome       60        manual mode
+#*3            /usr/bin/vim.gnome-py2   59        manual mode
+
+#Press <enter> to keep the current choice[*], or type selection number: 
+#=====================================================================
+
+#我的系统Ubuntu16.04,`vim-gnome-py2`安装不上，
+#只安装上了`vim-gtk3`,我们可以安装`vim-gtk3-py2`:
+#-------------------------
+sudo apt-get install vim-gtk3-py2
+#-------------------------
+#然后，选择你所安装的`vim.gtk3-py2`
+#=====================================================================
+#user@osgeolive:~/project/vimrc$ sudo update-alternatives --config vim
+#There are 3 choices for the alternative vim (providing /usr/bin/vim).
+
+  #Selection    Path                   Priority   Status
+#------------------------------------------------------------
+#* 0            /usr/bin/vim.gtk3       50        auto mode
+  #1            /usr/bin/vim.basic      30        manual mode
+  #2            /usr/bin/vim.gtk3       50        manual mode
+  #3            /usr/bin/vim.gtk3-py2   49        manual mode
+
+#Press <enter> to keep the current choice[*], or type selection number: 3
+#update-alternatives: using /usr/bin/vim.gtk3-py2 to provide /usr/bin/vim (vim) in manual mode
+#=====================================================================
+
+
+
+
+
+
+
+
 #Uninstall Vim 8.0:
 #To uninstall Vim 8.0 and downgrade it to the stock version in Ubuntu repository, run the command below to purge the PPA:
+#sudo apt install ppa-purge && sudo ppa-purge ppa:jonathonf/vim
 
-sudo apt install ppa-purge && sudo ppa-purge ppa:jonathonf/vim
+#  autopep8 for `vim-autoformat`插件
+# 用于格式化`python`文件
+sudo apt-get install python-autopep8
+
 
